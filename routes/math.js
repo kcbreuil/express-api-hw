4a7a6dd0d06d5f7d4f22a17b5a983a45dc40009d
Katie Breuil <kcbreuil@gmail.com>
2020-06-02 19:09:57 -0400

    small fixes

--- routes/math.js
+++ routes/math.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+let count = 0;
+
 router.put("/count", (req, res) => {
   const { count } = req.body;
-  res.send({ message: `This route has been hit ${count}` });
+  res.send({ message: `This route has been hit ${count} times` });
 });
 
 router.delete("/reset", (req, res) => {
+  count = 0;
   res.send("The count has been reset");
 });
 
 router.post("/sum", (req, res) => {
-  const number = req.body.number;
+  const { number } = req.query;
   res.send(`The sum of ${number} and 7 is ${number + 7}`);
 });
 
 router.get("/subtract", (req, res) => {
-  const number = req.query.number;
+  const { number } = req.query;
   res.send(`${number} - 7 is ${number - 7}.`);
 });
 
